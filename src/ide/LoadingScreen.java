05f165c84b976e85d790fef38ab4529e60076ec0
mike <mike@Mike-Ubuntu>
2015-05-11 14:23:01 -0700

    Forgot to remove dead code.

--- src/ide/LoadingScreen.java
+++ src/ide/LoadingScreen.java
@@ -1,6 +1,5 @@
 package ide;
 
-import java.awt.Graphics2D;
 import java.awt.SplashScreen;
 import java.io.IOException;
 import java.net.URL;
@@ -10,14 +9,11 @@ public class LoadingScreen {
 	public void launch() {
 
 		SplashScreen screen = SplashScreen.getSplashScreen();
-		Graphics2D g = screen.createGraphics();
-		
-		System.out.println(this.getClass().getClassLoader().getResource("resources/splash.gif"));
+//		System.out.println(this.getClass().getClassLoader().getResource("resources/splash.gif"));
 		
 		try {
 			URL splashPath = this.getClass().getClassLoader().getResource("resources/splash.gif");
 			screen.setImageURL(splashPath);
-			int q = 0;
 		} catch (NullPointerException | IllegalStateException | IOException e) {
 			e.printStackTrace();
 		}
