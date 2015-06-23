e0a5e8cc257b18b0543f30f8967da3c2a4d44d19
Mike Nowicki <mikenowicki022@outlook.com>
2015-06-23 11:38:26 -0700

    More library stuff, added more packages.

--- src/ide/packages/LibraryPackages.java
+++ src/ide/packages/LibraryPackages.java
@@ -2,6 +2,15 @@ package ide.packages;
 
 public interface LibraryPackages {
 
-	public static final String[] libraries = { "networxlib.components", "networxlib.connectivity", "networxlib.examples", "networxlib.graphloader", "networxlib.search", "networxlib.search.tools", "networxlib.visualizer" };
+	public static final String[] libraries = { "networxlib.components", 
+											   "networxlib.connectivity", 
+											   "networxlib.examples", 
+											   "networxlib.graphloader", 
+											   "networxlib.graphloader.utils",
+											   "networxlib.search", 
+											   "networxlib.search.tools", 
+											   "networxlib.stocks", 
+											   "networxlib.visualizer",
+											   "networxlib.webcrawler" };
 	
 }
