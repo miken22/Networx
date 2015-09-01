a086814f07bb61f15b93212d5b536f6e0e7dc08b
Michael Nowicki <mikenowicki022@gmail.com>
2015-09-01 16:29:18 -0700

    Clear dead classes

--- src/main/com/ide/texteditor/TextEditorDocument.java
+++ src/main/com/ide/texteditor/TextEditorDocument.java
@@ -14,7 +14,8 @@ import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.StyleConstants;
 
 /**
- * The document that the user edits to create their scripts
+ * The document style for the scripts, this class defines the
+ * colouring of key words, quotes, and comments within the text.
  * 
  * @author Mike Nowicki
  *
@@ -22,22 +23,32 @@ import javax.swing.text.StyleConstants;
 public class TextEditorDocument extends DefaultStyledDocument {
 
 	private static final long serialVersionUID = 1L;
-	private SimpleAttributeSet reservedWords;
+	private SimpleAttributeSet reservedColour;
 	private SimpleAttributeSet defaultColour;
 	private SimpleAttributeSet quotations;
 	private SimpleAttributeSet comments;
 
 	private boolean hasChanged;
 
+	/**
+	 * Since these words are reserved an enum type cannot be used, instead string
+	 * comparisons must be done
+	 */
+	public static final String[] reservedWords = {
+			"private","public","protected","final","super","if","while","do","void"+
+			"true","null","false","else","System","static","throws","int","double" +
+			"float","byte","interface","new","boolean","class","for" 
+			};
+	
 	public TextEditorDocument() {
 
 		// Initialize all the different styles for text highlighting.
-		reservedWords = new SimpleAttributeSet();
+		reservedColour = new SimpleAttributeSet();
 		defaultColour = new SimpleAttributeSet();
 		quotations = new SimpleAttributeSet();
 		comments = new SimpleAttributeSet();
 
-		StyleConstants.setForeground(reservedWords, Color.RED);
+		StyleConstants.setForeground(reservedColour, Color.RED);
 		StyleConstants.setForeground(defaultColour, Color.BLACK);
 		StyleConstants.setForeground(quotations, Color.BLUE);
 		StyleConstants.setForeground(comments, Color.GREEN);
@@ -147,13 +158,49 @@ public class TextEditorDocument extends DefaultStyledDocument {
 		colourQuotes();
 		colourComments();
 	}
+
+	private void updateTextStyles() throws BadLocationException {
+
+		// Clear existing styles
+		this.setCharacterAttributes(0, getText(0, getLength()).length(), defaultColour, true);
+		
+		Pattern pattern = buildPattern();
+
+		// Look for tokens and highlight them
+		Matcher matcher = pattern.matcher(getText(0, getLength()));
+		while (matcher.find()) {
+			// Change the color of recognized tokens
+			this.setCharacterAttributes(matcher.start(), matcher.end() - matcher.start(), reservedColour, false);
+		}
+	}
 	
+	/**
+	 * Builds a regex pattern of all words to search for
+	 * @return The regex experession
+	 */
+	private Pattern buildPattern() {
+
+		StringBuilder sb = new StringBuilder();
+		for (String token : reservedWords) {
+			sb.append("\\b"); // Start of word boundary
+			sb.append(token);
+			sb.append("\\b|"); // End of word boundary and an or for the next word
+		}
+		// Remove the trailing "|"
+		if (sb.length() > 0) {
+			sb.deleteCharAt(sb.length() - 1);
+		}
+
+		return Pattern.compile(sb.toString());
+
+	}
+
 	/*
 	 * The following are used to update which style to use
 	 */
 
 	public void setReservedWords(SimpleAttributeSet reservedWords) {
-		this.reservedWords = reservedWords;
+		this.reservedColour = reservedWords;
 	}
 
 	public void setDefaultColour(SimpleAttributeSet defaultColour) {
@@ -183,40 +230,4 @@ public class TextEditorDocument extends DefaultStyledDocument {
 		hasChanged = false;
 	}
 
-	/**
-	 * Builds a regex pattern of all words to search for
-	 * @return The regex experession
-	 */
-	private Pattern buildPattern() {
-
-		StringBuilder sb = new StringBuilder();
-		for (String token : ReservedWords.reservedWords) {
-			sb.append("\\b"); // Start of word boundary
-			sb.append(token);
-			sb.append("\\b|"); // End of word boundary and an or for the next word
-		}
-		// Remove the trailing "|"
-		if (sb.length() > 0) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
-
-		return Pattern.compile(sb.toString());
-
-	}
-
-
-	private void updateTextStyles() throws BadLocationException {
-
-		// Clear existing styles
-		this.setCharacterAttributes(0, getText(0, getLength()).length(), defaultColour, true);
-		
-		Pattern pattern = buildPattern();
-
-		// Look for tokens and highlight them
-		Matcher matcher = pattern.matcher(getText(0, getLength()));
-		while (matcher.find()) {
-			// Change the color of recognized tokens
-			this.setCharacterAttributes(matcher.start(), matcher.end() - matcher.start(), reservedWords, false);
-		}
-	}
 }
